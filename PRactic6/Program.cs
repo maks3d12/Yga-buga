e880f650bf790934016f703a4b45c41d34b54972
maks3d12 <maks-2005-10@mail.ru>
2022-11-11 21:05:08 +0300

    first commit

---
+++ PRactic6/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.IO;
+using Save;
+namespace Redactor
+{
+    class redactor
+    {
+        static void Main()
+        {
+            bool k = true;
+            string path;
+            while (k)
+            {
+               
+                Console.WriteLine("Добро пожаловать в редактор");
+                Console.WriteLine("Выбирете путь до файла \t Чтобы выйти нажмите Escape");
+                ConsoleKeyInfo key = Console.ReadKey();
+                 path = Console.ReadLine();
+                path = ("C" + path);  // костыль.У меня почему то непередавался диск, хотя вводил я его вверно, пришлось таким способом добовлять
+                Text_text text = new Text_text();
+                text.text = File.ReadAllText(path);
+                Info(text, path);
+                if (key.Key == ConsoleKey.Escape)
+                {
+                    Console.WriteLine("Вы вышли из программы");
+                    k = false;
+                }
+            }
+        }
+        static void Info(Text_text text, string path)
+        {
+            Console.Clear();
+            Console.WriteLine("Если хотите сохранить текст, нажмите F1");
+            Console.WriteLine(text.text);
+            bool k = true;
+            while (k)
+            {
+                ConsoleKeyInfo key = Console.ReadKey();
+                if (key.Key == ConsoleKey.F1)
+                {
+                    k = false;
+                    Saveworld.gaga(text, path);
+                }
+                if (key.Key == ConsoleKey.Escape)
+                {
+                    k = false;
+                }
+            }
+        }
+    }
+
+
+}
+
+
+
+
---
+++ PRactic6/static void Main().cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Save
+{
+    public class Text_text
+    {
+        public string text;
+    }
+    internal class Saveworld
+    {
+
+        public static void gaga(Text_text text, string path)
+        {
+            text.text = Deserialized(text, path);
+            Console.Clear();
+            Console.WriteLine("Куда вы хотите сохранит файл?");
+            string safeWork = Console.ReadLine();
+            string[] word = safeWork.Split(".");
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i] == "txt")
+                {
+                    File.WriteAllText(safeWork, text.text);
+
+                }
+                if (word[i] == "xml")
+                {
+                   
+                    XmlSerializer xml = new XmlSerializer(typeof(Text_text));
+                    using (FileStream fs = new FileStream(safeWork, FileMode.OpenOrCreate))
+                    {
+                        xml.Serialize(fs, text);
+                    }
+
+                }
+                if (word[i] == "json")
+                {
+                    string json = JsonConvert.SerializeObject(text);
+                    File.WriteAllText(safeWork, json);
+                }
+            }
+        }
+     private static string Deserialized(Text_text text, string path)
+        { 
+            Console.Clear();
+            string error = "Вы неправильно ввели формат";
+            string[] word = path.Split(".");
+            for (int i = 0; i < word.Length; i++)
+            {
+
+                if (word[i] == "xml")
+                {
+                    XmlSerializer xml = new XmlSerializer(typeof(Text_text));
+                    using (FileStream fs = new FileStream(path, FileMode.Open))
+                    {
+                        Text_text? text_text = (Text_text)xml.Deserialize(fs);
+                        return text_text.text;
+                    }
+                }
+                if (word[i] == "json")
+                {
+                    string result = Convert.ToString(JsonConvert.DeserializeObject(text.text));
+                    return result;
+                }
+                if (word[i] == "txt")
+                {
+                    return text.text;
+                }
+                
+               
+            }
+            return error;
+        }
+    }
+   
+
+}
